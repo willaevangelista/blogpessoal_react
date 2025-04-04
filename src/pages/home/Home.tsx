531cfc333470921ef6031bf1326cf35458b3b156
Willa Evangelista <willaevangelista.pro@gmail.com>
2025-04-04 10:34:28 -0300

    Inclusão do framework Tailwind CSS

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -3,31 +3,17 @@ import React from 'react'
 function Home() {
     return (
         <>
-            <div style={{
-                width: "100vw",
-                display: "flex",
-                justifyContent: "center"
-            }}>
+            <div className="w-screen flex justify-center">
                 <div>
-                    <div style={{
-                         width: "80vw",
-                         display: "flex",
-                         flexDirection: "column",
-                         alignItems: "center"
-                    }}>
+                    <div className="max-w-7xl flex flex-col items-center">
                         <h2>Seja Bem Vinde!</h2>
                         <p>Expresse aqui seus pensamentos e opniões</p>
                     </div>
 
-                    <div style={{
-                         width: "80vw",
-                         display: "flex",
-                         flexDirection: "column",
-                         alignItems: "center"
-                    }}>
-                        <img 
-                            src="https://i.imgur.com/VpwApCU.png" 
-                            alt="Imagem da Página Home" 
+                    <div className="max-w-7xl flex flex-col items-center">
+                        <img
+                            src="https://i.imgur.com/VpwApCU.png"
+                            alt="Imagem da Página Home"
                             width="400px"
                         />
                     </div>
