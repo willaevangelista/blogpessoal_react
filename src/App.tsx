c09a90982b0e477c67550d8206c97a294fb88b26
Willa Evangelista <willaevangelista.pro@gmail.com>
2025-04-07 11:48:09 -0300

    Inclusão dos componetes Navbar e Footer no React, bem como atualização da Home.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,25 @@
-import Home from './pages/home/Home'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Footer from './components/footer/Footer';
+import Navbar from './components/navbar/Navbar';
+import Home from './pages/home/Home';
+import './App.css';
+
 
 function App() {
   return (
-    <Home />
-  );
+    <>
+        <BrowserRouter>
+          <Navbar />
+          <div className="min-h-[80vh]">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/home" element={<Home />} />
+            </Routes>
+          </div>
+          <Footer />
+        </BrowserRouter>
+    </>
+  )
 }
 
 export default App;
